391dc6f484cc004ec839c5aaef1e7eb229434cf7
Hamdi Ismail <hamdihismail@gmail.com>
2024-02-09 02:36:16 -0500

    lab_3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,47 +2,43 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnNext = (Button)findViewById(R.id.btnNext);
+    EditText edtText = (EditText)findViewById(R.id.edtText);
+    SharedPreferences prefs = getSharedPreferences("shared", Context.MODE_PRIVATE);
+    SharedPreferences.Editor edit = prefs.edit();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        edit.putString("edtText", String.valueOf(edtText.getText()));
+        edit.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
 
-        Button btnPressMe = (Button)findViewById(R.id.btnPressMe);
-        TextView textView = (TextView)findViewById(R.id.textview);
-        EditText edtText = (EditText)findViewById(R.id.edtText);
-        String toast_message = (String)getResources().getString(R.string.toast_message);
-        String snackBar_message = (String)getResources().getString(R.string.snackBar_message);
-        String on = (String)getResources().getString(R.string.on);
-        String off = (String)getResources().getString(R.string.off);
-        CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+        Intent nextPage = new Intent(this, NameActivity.class);
 
-        btnPressMe.setOnClickListener( (click) -> {
-            textView.setText(edtText.getText());
-            Toast.makeText(MainActivity.this,toast_message,Toast.LENGTH_LONG).show();
-        });
+        if(prefs.contains("edtText")){
+            edtText.setText(prefs.getInt("edtText",0));
+        }
 
-        checkBox.setOnCheckedChangeListener((cb, isChecked) -> {
-            String onOff;
-            if(isChecked){
-                onOff = " " + on;
-            } else {
-                onOff = " " + off;
-            }
-            Snackbar.make(checkBox, snackBar_message + onOff, Snackbar.LENGTH_LONG)
-                .setAction( "Undo", click -> cb.setChecked(!isChecked))
-                .show();
+
+
+
+        btnNext.setOnClickListener((click) -> {
+            nextPage.putExtra("typed", edtText.getText());
+            startActivityForResult(nextPage,1);
         });
     }
 }
