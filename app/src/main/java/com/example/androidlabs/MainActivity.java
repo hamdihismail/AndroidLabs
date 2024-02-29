c30442c015d2395561962b4601aa6c9647245115
Hamdi Ismail <hamdihismail@gmail.com>
2024-02-29 07:01:32 -0500

    lab_6

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,19 +3,29 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InputStream is = getResources().openRawResource(+R.drawable.test);
+        bitmap = BitmapFactory.decodeStream(is);
+        imageView = findViewById(R.id.imgView);
+        imageView.setImageBitmap(bitmap);
     }
 
     public class CatImages extends AsyncTask<String, Integer, String>{
-//        private Bitmap currentImg = new Bitmap();
+
         @Override
         protected String doInBackground(String... strings) {
             return null;
