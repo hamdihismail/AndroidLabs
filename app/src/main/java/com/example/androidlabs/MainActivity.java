0047399619cfeb0ea624b1b571e1999056f749fd
Hamdi Ismail <hamdihismail@gmail.com>
2024-03-03 07:17:24 -0500

    lab_6

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -8,7 +8,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +24,99 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        HttpURLConnection connection = null;
+//        BufferedReader reader = null;
+
+        new CatImages().execute("https://cataas.com/cat?json=true");
+
+//        try {
+//            URL url = new URL("https://cataas.com/cat?json=true");
+//            connection = (HttpURLConnection) url.openConnection();
+//            connection.connect();
+//
+//            InputStream stream = connection.getInputStream();
+//
+//
+//            reader = new BufferedReader(new InputStreamReader(stream));
+//            StringBuffer buffer = new StringBuffer();
+//
+//            String line = "";
+//            while((line = reader.readLine()) != null){
+//                buffer.append(line);
+//            }
+//            System.out.println(buffer.toString());
+//
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }catch (IOException e) {
+//            e.printStackTrace();
+//        }finally {
+//            if(connection != null){
+//                connection.disconnect();
+//            }
+//            try {
+//                if(reader != null){
+//                    reader.close();
+//                }
+//            }catch (IOException e){
+//                e.printStackTrace();
+//            }
+//        }
+
         InputStream is = getResources().openRawResource(+R.drawable.test);
         bitmap = BitmapFactory.decodeStream(is);
         imageView = findViewById(R.id.imgView);
-        imageView.setImageBitmap(bitmap);
+
+//        imageView.setImageBitmap(bitmap);
     }
 
     public class CatImages extends AsyncTask<String, Integer, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(connection != null){
+                    connection.disconnect();
+                }
+                try {
+                    if(reader != null){
+                        reader.close();
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
             return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            System.out.println(result);
         }
     }
 }
