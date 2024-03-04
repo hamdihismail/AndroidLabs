d3f1e2ff3bf151e7e2aea1af5041866b43f62643
Hamdi Ismail <hamdihismail@gmail.com>
2024-03-04 06:24:58 -0500

    lab_6

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,6 +16,7 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -29,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private Bitmap bitmap;
     //Create Path to save Image
-    private File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES+ "/AndroidDvlpr"); //Creates app specific folder
+//    private File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES+ "/AndroidDvlpr"); //Creates app specific folder
+    private File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES+"/CatImages");
+
 
 
     @Override
@@ -44,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
 //        HttpURLConnection connection = null;
 //        BufferedReader reader = null;
 
-        new CatImages().execute("https://cataas.com/cat?json=true");
+//        new CatImages().execute("https://cataas.com/cat?json=true");
+
 
 //        try {
 //            URL url = new URL("https://cataas.com/cat?json=true");
@@ -80,70 +86,119 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }
 
-        InputStream is = getResources().openRawResource(+R.drawable.test);
-        bitmap = BitmapFactory.decodeStream(is);
+//        InputStream is = getResources().openRawResource(+R.drawable.test);
+        File test = new File("/sdcard/Pictures/CatImages/1Y3dpssxcbHPEkfO.png");
+        if(test.exists()){
+            System.out.println("Test File Exists!!!");
+        }
+        bitmap = BitmapFactory.decodeFile(test.toString());
         imageView = findViewById(R.id.imgView);
 
-//        imageView.setImageBitmap(bitmap);
+        imageView.setImageBitmap(bitmap);
     }
 
     public class CatImages extends AsyncTask<String, Integer, String>{
+        String newImgFile;
 
         @Override
         protected String doInBackground(String... params) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
+            FileOutputStream out;
+            while(true) {
+                try {
+                    URL url = new URL(params[0]);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
 
-                InputStream stream = connection.getInputStream();
+                    InputStream stream = connection.getInputStream();
 
 
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
 
-                String line = "";
-                while((line = reader.readLine()) != null){
-                    buffer.append(line);
-                }
-                String finalJson = buffer.toString();
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+                    String finalJson = buffer.toString();
 
-                JSONObject parentObject = new JSONObject(finalJson);
-                String id = parentObject.get("_id").toString();
-                Path oldFile = Paths.get(id+".png");
+                    JSONObject parentObject = new JSONObject(finalJson);
+                    String id = parentObject.get("_id").toString();
+//                    Path oldFile = Paths.get(id + ".png");
+                    File oldFile = new File("/sdcard/Pictures/CatImages/"+id+".png");
 
-                if (Files.exists(oldFile)){
-                    System.out.println("The file exists!!");
-                }
+                    if (oldFile.exists()) {
+                        System.out.println("The file exists!!");
+                        newImgFile = oldFile.toString();
+                    } else {
+                        System.out.println("The file doesn't exist!!");
+                        URL imgURL = new URL("https://cataas.com/cat?id=" + id);
+                        bitmap = BitmapFactory.decodeStream(imgURL.openConnection().getInputStream());
+                        File imageFile = new File(path, String.valueOf(id) + ".png"); // Imagename.png
+                        newImgFile = imageFile.toString();
+                        out = new FileOutputStream(imageFile);
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // Compress Image
+                        out.flush();
+                        out.close();
 
-                return id;
+                        System.out.println("FIle Saved!!");
+                        System.out.println("Path: " + path.toString());
+                        MediaScannerConnection.scanFile(MainActivity.this, new String[]{imageFile.getAbsolutePath()}, null, new MediaScannerConnection.OnScanCompletedListener() {
+
+                            public void onScanCompleted(String path, Uri uri) {
+                                // Log.i("ExternalStorage", "Scanned " + path + ":");
+                                //    Log.i("ExternalStorage", "-> uri=" + uri);
+                            }
+                        });
+                        for (int i = 0; i < 100; i++) {
+                            try {
+                                publishProgress(i);
+                                Thread.sleep(30);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+                    }
+
+                    return id;
 
 //                return buffer.toString();
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } finally {
-                if(connection != null){
-                    connection.disconnect();
-                }
-                try {
-                    if(reader != null){
-                        reader.close();
-                    }
-                }catch (IOException e){
+                } catch (MalformedURLException e) {
                     e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                    try {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+                return null;
             }
-            return null;
         }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            bitmap = BitmapFactory.decodeFile(newImgFile);
+            imageView.setImageBitmap(bitmap);
+        }
+
         @Override
         protected void onPostExecute(String result){
+            imageView = findViewById(R.id.imgView);
             super.onPostExecute(result);
             System.out.println(result);
         }
